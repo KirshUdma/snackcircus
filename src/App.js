9749b5b35b7dcfb9d1a939036da8d0a314e5f8de
Krish Udma <krishudma@gmail.com>
2021-11-02 17:51:32 +0400

    added auth v3

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          visit us @ snackcircus.com
+          visit us @snackcircus.com
         </a>
       </header>
       <AmplifySignOut />
